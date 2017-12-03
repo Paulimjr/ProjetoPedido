f4b67be272e9534fc1d4e8889c3981c579b7b9ce
PaulimJr <paulim.jr@hotmail.com>
2017-12-03 20:22:22 -0200

    Personalizando o retorno da validacao das API

--- src/main/java/br/com/projeto/pedido/service/exception/ResponseExceptionHandler.java
+++ src/main/java/br/com/projeto/pedido/service/exception/ResponseExceptionHandler.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -48,4 +50,25 @@ public class ResponseExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
+	/**
+	 * Response personalizado para erro de validação
+	 * 
+	 * @param e
+	 * @param request
+	 * @return
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		LocalDate date = LocalDate.now();
+		String dataAtual = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", dataAtual);
+		
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
