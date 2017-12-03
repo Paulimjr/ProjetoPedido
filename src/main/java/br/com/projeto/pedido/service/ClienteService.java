1561eda0f5d94d573b55f75a922c9452b215b2bf
PaulimJr <paulim.jr@hotmail.com>
2017-12-03 21:04:36 -0200

    Ajustando e testando - DELETE clientes cadastrados

--- src/main/java/br/com/projeto/pedido/service/ClienteService.java
+++ src/main/java/br/com/projeto/pedido/service/ClienteService.java
@@ -81,7 +81,7 @@ public class ClienteService {
 			clienteRepository.delete(id);
 		}
 		catch (DataIntegrityException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
