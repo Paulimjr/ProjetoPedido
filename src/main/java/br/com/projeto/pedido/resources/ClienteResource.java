594b0572700bd99c4d056eac314c45ada7cddf37
PaulimJr <paulim.jr@hotmail.com>
2017-12-03 20:40:04 -0200

    ENPOINT para cliente PUT, DELETE e GET's

--- src/main/java/br/com/projeto/pedido/resources/ClienteResource.java
+++ src/main/java/br/com/projeto/pedido/resources/ClienteResource.java
@@ -1,12 +1,21 @@
 package br.com.projeto.pedido.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.pedido.dto.ClienteDTO;
 import br.com.projeto.pedido.entity.Cliente;
 import br.com.projeto.pedido.service.ClienteService;
 /**
@@ -33,5 +42,68 @@ public class ClienteResource {
 	public ResponseEntity<?> buscarClientePeloId(@PathVariable final Integer id) {
 		Cliente cli = this.clienteService.buscar(id);
 		return  ResponseEntity.ok().body(cli);
+	}
+	
+	/**
+	 * API para alterar dados de um cliente
+	 * 
+	 * @param objDto
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO cliDto, @PathVariable Integer id) {
+		Cliente cli = clienteService.fromDTO(cliDto);
+		cli.setId(id);
+		
+		cli = clienteService.update(cli);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	/**
+	 * API para remover um cliente
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	/**
+	 * API para trazer todos os clientes
+	 * 
+	 * @return
+	 */
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = clienteService.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());  
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	/**
+	 * API para consulta cliente com paginação
+	 * 
+	 * @param page
+	 * @param linesPerPage
+	 * @param orderBy
+	 * @param direction
+	 * @return
+	 */
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		
+		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/br/com/projeto/pedido/service/ClienteService.java
+++ src/main/java/br/com/projeto/pedido/service/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.projeto.pedido.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.projeto.pedido.dto.ClienteDTO;
 import br.com.projeto.pedido.entity.Categoria;
 import br.com.projeto.pedido.entity.Cliente;
 import br.com.projeto.pedido.repository.CategoriaRepository;
 import br.com.projeto.pedido.repository.ClienteRepository;
+import br.com.projeto.pedido.service.exception.DataIntegrityException;
 import br.com.projeto.pedido.service.exception.ObjectNotFoundException;
 
 /**
@@ -37,5 +44,78 @@ public class ClienteService {
 		}
 		
 		return cli;
+	}
+	
+	/**
+	 * Atualizar informaçoes de um cliente
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.buscar(obj.getId());
+		updateData(newObj, obj);
+		return clienteRepository.save(newObj);
+	}
+	
+	/**
+	 * Remover um cliente cadastrado
+	 * 
+	 * @param id
+	 */
+	public void delete(Integer id) {
+		
+		this.buscar(id);
+		
+		try {
+			clienteRepository.delete(id);
+		}
+		catch (DataIntegrityException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	/**
+	 * Trazer todos os clientes cadastrados
+	 * 
+	 * @return
+	 */
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+	
+	/**
+	 * Consulta cliente com paginação
+	 * 
+	 * @param page
+	 * @param linesPerPage
+	 * @param orderBy
+	 * @param direction
+	 * @return
+	 */
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	/**
+	 * Converter ClienteDTO para entidade Cliente
+	 * 
+	 * @param objDto
+	 * @return
+	 */
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	/**
+	 * Atualizar os parametros
+	 * 
+	 * @param newObj
+	 * @param obj
+	 */
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
